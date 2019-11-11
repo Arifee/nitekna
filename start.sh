3f167899ce59d15b8640dc4ea148576ffaf5237a
Arife Ljutviovska <38080549+Arifee@users.noreply.github.com>
2019-11-11 12:59:55 +0100

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-exec /opt/codedeploy-agent/deployment-root/afcf245c-0c08-4259-93d0-f81569a5d09d/d-P6BYAAYIO/logs/scripts.sh
 
 cd /home/ec2-user/server
 sudo /usr/bin/java -jar -Dserver.port=80 \
